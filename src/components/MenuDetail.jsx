6f54a70bd642f1449e795877671343cb9d196465
Beto <betoml5@hotmail.com>
2021-06-22 17:05:43 -0500

    Dummy change

--- src/components/MenuDetail.jsx
+++ src/components/MenuDetail.jsx
@@ -1,13 +1,13 @@
-import menuImage from "../static/menu-test.png";
+import menuImage from "../static/restaurantMenu.jpg";
 
-export const MenuDetail = ({}) => {
+export const MenuDetail = (props) => {
   return (
     <div className="flex flex-col blur w-5/6 justify-self-center mt-4 p-8 max-w-sm relative">
       <picture className="flex flex-col ">
-        <img src={menuImage} alt="" />
         <button className="absolute centerBtnMenu uppercase font-thin text-white bg-lightDark py-2  px-2 rounded-full bottom-4">
           Ver menu
         </button>
+        <img src={menuImage} alt="restaurantImage" className="w-full" />
       </picture>
 
       <button className="bg-lightGreen px-2 py-2 rounded-full mt-6 font-monse text-sm">
