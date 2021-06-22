030ac1b68ff1ada8c65fffcf209cedbf05efe3cd
Beto <betoml5@hotmail.com>
2021-06-22 13:54:04 -0500

    Add left & right arrows menu

--- src/components/ShopDetail.jsx
+++ src/components/ShopDetail.jsx
@@ -1,5 +1,7 @@
 import { Shop } from "./Shop";
 import pizzaIcon from "../static/pizza-test.png";
+import leftArrow from "../static/left-arrow.svg";
+import rightArrow from "../static/right-arrow.svg";
 
 export const ShopDetail = () => {
   return (
@@ -7,9 +9,24 @@ export const ShopDetail = () => {
       <Shop />
       <div className="flex flex-col items-center blur w-5/6 h-52 bg-white  justify-self-center">
         <h3 className="text-center mt-4 uppercase text-xl">Menu</h3>
-        <picture className="mt-5">
-          <img src={pizzaIcon} alt="pizzaIcon" className="w-34" />
-        </picture>
+
+        <div className="flex items-center mt-4">
+          <div>
+            <picture>
+              <img src={leftArrow} alt="" />
+            </picture>
+          </div>
+          <div className="mx-4">
+            <picture className="">
+              <img src={pizzaIcon} alt="pizzaIcon" className="w-34" />
+            </picture>
+          </div>
+          <div>
+            <picture>
+              <img src={rightArrow} alt="" />
+            </picture>
+          </div>
+        </div>
       </div>
     </div>
   );
