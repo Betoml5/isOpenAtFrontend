f6d44a474d23e92a455a5efcf5f306cf136ec140
Beto Martinez <betoml5@hotmail.com>
2021-07-30 16:07:05 -0500

    Fix marker map

--- src/containers/Location.jsx
+++ src/containers/Location.jsx
@@ -1,18 +1,29 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
+import L from 'leaflet';
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
+    iconUrl: require('leaflet/dist/images/marker-icon.png').default,
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png').default
+});
+
+
+const position = [27.9324, -101.2157];
 
 const Location = () => {
     return (
-        <MapContainer center={[27.9286, -101.2150]} zoom={13} scrollWheelZoom={false} style={{ height: "100vh" }} >
+        <MapContainer center={[27.9286, -101.2150]} zoom={15} scrollWheelZoom={false} style={{ height: "100vh" }}    >
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
+            <Marker position={position}>
                 <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
+                    Here you are
                 </Popup>
             </Marker>
         </MapContainer>
