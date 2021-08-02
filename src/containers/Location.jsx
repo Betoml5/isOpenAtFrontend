b9e016fa425f5181daf62d80420b26f9139badb3
Beto <betoml5@hotmail.com>
2021-08-01 20:41:24 -0500

    Change map style

--- src/containers/Location.jsx
+++ src/containers/Location.jsx
@@ -34,8 +34,8 @@ const Location = () => {
       zoomAnimation={true}
     >
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
+        url="https://api.mapbox.com/styles/v1/betoml5/ckrtydjfigv4t19og4jequqbd/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYmV0b21sNSIsImEiOiJja3J0eWp0NTkwdTltMndxbWdrczJpbnNoIn0.iq1W0M6Pq4_9WrlZq2rhTg"
       />
 
       {shops?.map((shop) => (
