e1b039e0f93d759060c289e7f0078be6c22fde3c
Beto <betoml5@hotmail.com>
2021-08-01 20:53:06 -0500

    Fix z-index styles map

--- src/containers/Location.jsx
+++ src/containers/Location.jsx
@@ -1,7 +1,6 @@
 import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L, { Popup } from "leaflet";
-import { useCallback } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { getShops } from "../services/Shop";
@@ -26,22 +25,24 @@ const Location = () => {
   }, []);
 
   return (
-    <MapContainer
-      center={[27.8617, -101.1255]}
-      zoom={15}
-      scrollWheelZoom={true}
-      style={{ height: "90vh" }}
-      zoomAnimation={true}
-    >
-      <TileLayer
-        attribution="© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
-        url="https://api.mapbox.com/styles/v1/betoml5/ckrtydjfigv4t19og4jequqbd/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYmV0b21sNSIsImEiOiJja3J0eWp0NTkwdTltMndxbWdrczJpbnNoIn0.iq1W0M6Pq4_9WrlZq2rhTg"
-      />
+    <div className="h-screen">
+      <MapContainer
+        center={[27.8617, -101.1255]}
+        zoom={15}
+        scrollWheelZoom={true}
+        style={{ height: "100%", zIndex: 10 }}
+        zoomAnimation={true}
+      >
+        <TileLayer
+          attribution="© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
+          url="https://api.mapbox.com/styles/v1/betoml5/ckrtydjfigv4t19og4jequqbd/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYmV0b21sNSIsImEiOiJja3J0eWp0NTkwdTltMndxbWdrczJpbnNoIn0.iq1W0M6Pq4_9WrlZq2rhTg"
+        />
 
-      {shops?.map((shop) => (
-        <Marker position={shop?.location}></Marker>
-      ))}
-    </MapContainer>
+        {shops?.map((shop) => (
+          <Marker position={shop?.location}></Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
 };
 
